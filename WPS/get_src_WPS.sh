3337511b50a7672bf84a602fe3f0742009825cbf
Kadir Akbudak <kadir.cs@gmail.com>
2024-10-10 17:01:48 +0300

    Create get_src_WPS.sh

---
+++ WPS/get_src_WPS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function usage() {
+   echo "Usage: $0 version"
+   echo "       version in format X.Y.Z"
+   exit 0
+}
+if [[ $@ == "--help" ||  $@ == "-h" ]]; then
+        usage
+fi
+if [ $# -ne 1 ] ; then
+    usage
+else
+    version=v$1
+fi
+git clone --branch $version https://github.com/wrf-model/WPS.git
