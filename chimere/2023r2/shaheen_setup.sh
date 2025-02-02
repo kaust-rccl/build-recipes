955dfdfc5194de3e2020e064b50f3edd0ea52116
mshaikh786 <mohsinshaikh786@gmail.com>
2025-02-02 16:10:01 +0300

    fixing ungrib.exe issue with jpc_decode undefined

--- chimere/2023r2/shaheen_setup.sh
+++ chimere/2023r2/shaheen_setup.sh
@@ -17,6 +17,8 @@ cp configure.wrf.shaheen.gnu $MYCHIMERE/config_wrf433/
 cp configure.xios.shaheen.gnu $MYCHIMERE/config_xios/
 cp Makefile.hdr.shaheen.gnu $MYCHIMERE/makefiles.hdr/
 cp mychimere-shaheen.gnu $MYCHIMERE/
+patch -p 1 ${CHIMERE_ROOT}/WPS4.1/ungrib/src/ngl/g2/dec_jpeg2000.c ./dec_jpeg2000.patch
+
 
 cp make_CHIMERE $OASIS_DIR/util/make_dir/
 cp make.inc $OASIS_DIR/util/make_dir/ 
